657157cf90e3615cb3ddb3e8abfc45bb328d643d
Stephan Schöpe <socialcrap+github@posteo.de>
2023-08-30 22:57:21 +0200

    sentry added

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,26 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Fallback from "./views/404";
 import "./index.css";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://a421464f844af1763721fc9697834926@o4504899889397760.ingest.sentry.io/4505793070563328",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: [
+        "localhost",
+        "https://position.stephanschoepe.de",
+      ],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const router = createBrowserRouter([
   {
