7cbef1bc35c9ae0c10454d4ecfdc69b51fcdbee5
Stephan Schöpe <socialcrap+github@posteo.de>
2023-09-13 08:12:47 +0200

    comment added

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { target: "es2019" },
+  build: { target: "es2019" }, // Required for older Android WebView versions
   plugins: [react()],
 });
