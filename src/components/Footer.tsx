d9dfba08b6c6f0c5bde7530a92bd607c80a0b206
Stephan Schöpe <socialcrap+github@posteo.de>
2023-09-18 11:58:13 +0200

    link text decoration added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,13 +6,18 @@ export function Footer() {
       <div className="mb-2 flex items-center justify-center gap-2">
         <a
           href="https://github.com/JEK58/NoPressure"
-          className="flex items-center"
+          className="flex items-center decoration-dotted hover:underline"
         >
           <FaGithub className="mr-1" />
           Made with ❤️ by Stephan Schöpe
         </a>
         <span>|</span>
-        <a href="https://www.stephanschoepe.de/impressum">Impressum</a>
+        <a
+          href="https://www.stephanschoepe.de/impressum"
+          className="decoration-dotted hover:underline"
+        >
+          Impressum
+        </a>
       </div>
     </footer>
   );
