cbd850e1b443889124569ffc810462f63a4e77cb
Stephan Schöpe <socialcrap+github@posteo.de>
2023-09-18 09:00:42 +0200

    how to updated

--- src/components/HowToUse.tsx
+++ src/components/HowToUse.tsx
@@ -2,17 +2,21 @@ const HowToUse = () => {
   return (
     <div className="mt-1 max-w-2xl rounded-xl bg-white/10 p-4 text-white">
       <h3 className="text-lg font-semibold">How to Use</h3>
+      <ul className="ml-4 mt-3 list-outside list-disc text-gray-200">
+        <li>Pick a platform, comp and pilot and copy the generated URL.</li>
+        <li>
+          In XCTrack or Alfapilot look for the &quot;website widget&quot; (at
+          the bottom of the list) and paste the URL.
+        </li>
+      </ul>
       <p className="mt-2 text-gray-200">
-        Pick a platform, comp and pilot and copy the generated URL. In XCTrack
-        or Alfapilot look for the &quot;website widget&quot; (at the bottom of
-        the list) and paste the URL. <br />
         The widget will refresh every minute in the background. No need to set a
         refresh rate in the widget options. If the position data is older than 3
         minutes (e.g. bad connection in the air) the digits turn red.
         <br />
         Flyevent or PWC comps will also appear in the Flymaster groups but
         without respecting leading points* and no live score. So only use the
-        flymaster group if the comp is not available on the other platforms.
+        flymaster group if a comp is not available on the other platforms.
       </p>
       <h4 className="mt-3 text-lg font-semibold">Limitations</h4>
 
@@ -20,10 +24,11 @@ const HowToUse = () => {
         <li>This is a beta. Feel free to report any issues or suggestions!</li>
         <li>
           Some events may not appear before the first task is started or live
-          scoring is running. But if you have close a look at the generated URLs
+          scoring is running. But if you have a close look at the generated URLs
           you could always manually create your personal URL if you know your
           pilot number and the comp id.
         </li>
+        <li>Flymaster groups have a 4 minutes delay.</li>
         <li>
           With the recent changes in the PWC some adjustements may be needed
           during the event and the widget might not work at all times.
