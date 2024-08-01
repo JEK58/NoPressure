c87340b8fac7af396b12f0bb24e61f2d71c0a2fc
Stephan Schöpe <socialcrap+github@posteo.de>
2024-08-01 10:03:42 +0200

    h1 wrapped in link tag

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 
 import { Footer } from "@/components/Footer";
+import Link from "next/link";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -14,15 +15,16 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-start bg-gradient-to-b from-[#15162c] to-[#167caf]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <div>
-            <h1 className="max-w-2xl text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-              No<span className="text-[hsl(342,100%,70%)]">Pressure!</span>
-              <span className="ml-1 inline text-left text-sm tracking-normal">
-                beta
-              </span>
-            </h1>
-
+            <Link href="/">
+              <h1 className="max-w-2xl text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+                No<span className="text-[hsl(342,100%,70%)]">Pressure!</span>
+                <span className="ml-1 inline text-left text-sm tracking-normal">
+                  beta
+                </span>
+              </h1>
+            </Link>
             <h2 className="text-md mt-1 text-white md:text-xl">
               Race position widget URL generator for XCTrack & Alfapilot
             </h2>
